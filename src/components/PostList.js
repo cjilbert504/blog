86d93746e0e0912f971a666ec0f4610c7d5da5a6
Collin Jilbert <cjilbert504@gmail.com>
2020-08-06 14:41:17 -0500

    Add Alternative overfetching solution

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchPosts } from '../actions';
+import { fetchPostsAndUsers } from '../actions';
 import UserHeader from './UserHeader';
 
 class PostList extends React.Component {
     componentDidMount() {
-        this.props.fetchPosts();
+        this.props.fetchPostsAndUsers();
     }
 
     renderList() {
         return this.props.posts.map((post) => {
-            console.log(post);
             return (
                 <div className="item" key={post.id}>
                     <i className="large middle aligned icon user" />
@@ -37,4 +36,4 @@ const mapStateToProps = (state) => {
     return { posts: state.posts }
 }
 
-export default connect(mapStateToProps, { fetchPosts })(PostList);
+export default connect(mapStateToProps, { fetchPostsAndUsers })(PostList);
